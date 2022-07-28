52a2922c7654e6571295d5faa013abaf71ff5343
yuriykolomiyets <juriy121@gmail.com>
2022-07-28 12:48:58 +0200

    1

---
+++ w7/TestMyHashSet.java
@@ -0,0 +1,17 @@
+package aco.w7;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+
+public class TestMyHashSet {
+
+    public static void main(String[] args) {
+        Set<String> set = new MyHashSet<>();
+        set.add("some");
+        set.add("element");
+        set.add("ivan");
+
+
+    }
+}
